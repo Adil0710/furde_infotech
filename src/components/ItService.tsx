ef2917327bcc41d1816f04736f2840022be133f5
Adil0710 <padil2246@gmail.com>
2024-11-12 18:38:20 +0530

    done

--- src/components/ItService.tsx
+++ src/components/ItService.tsx
@@ -1,14 +1,28 @@
 import React from "react";
-import ServiceCards from "./ServiceCards";
+import ServiceCard from "./ServiceCard";
+import { itServices } from "@/lib/data";
 
 function ItService() {
   return (
     <div className="bg-white md:pl-20 md:pr-20 px-5 py-10 text-black">
-      <h1 className=" text-4xl font-bold leading-tight">
-      IT SERVICES
-      </h1>
-      <p className=" mt-8 text-[#4B5563]">Leveraging advanced technologies to propel your business growth.</p>
-      <ServiceCards/>
+      <h1 className=" text-4xl font-bold leading-tight">IT SERVICES</h1>
+      <p className=" mt-8 text-[#4B5563]">
+        Leveraging advanced technologies to propel your business growth.
+      </p>
+      <div className=" mt-10 grid grid-cols-3 gap-8">
+        {itServices.map((service, index) => {
+          return (
+            <ServiceCard
+              key={index}
+              image={service.image}
+              alt={service.title}
+              title={service.title}
+              description={service.description}
+              link={service.link}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/lib/data.ts
@@ -0,0 +1,121 @@
+import mobile from "@/assets/mobile.png";
+import mobilebg from "@/assets/mobilebg.png";
+import mobile1 from "@/assets/mobile1.png";
+import mobile2 from "@/assets/mobile2.png";
+import mobile3 from "@/assets/mobile3.png";
+import mobile4 from "@/assets/mobile4.png";
+import mobile5 from "@/assets/mobile5.png";
+import web from "@/assets/web.png";
+import backend from "@/assets/backend.png";
+import maintainance from "@/assets/maintainance.png";
+import consulting from "@/assets/consulting.png";
+import emerging from "@/assets/emerging.png";
+import testing from "@/assets/testing.png";
+import { ITService } from "@/types/types";
+
+export const itServices: ITService[]= [
+  {
+    image: mobile,
+    title: "Mobile App Development",
+    description:
+      "Custom mobile solutions for iOS, Android, and cross-platform needs.",
+    link: "/services/IT/mobile-app-development",
+    backgroundImage: mobilebg,
+    descriptionvm:
+      "Create custom mobile applications designed to meet unique business needs across iOS, Android, and cross-platform solutions. From intuitive UI/UX design to advanced functionalities, our mobile app services enhance user engagement and drive business growth across devices.",
+    cards: [
+      {
+        image: mobile1,
+        title: "Custom app Development",
+        descriptionLines: [
+          "Tailored app solutions designed to meet unique business objectives",
+          "Seamless integration with existing systems and workflows",
+          "Built with scalability and flexibility for future growth",
+          "Ensures high security and data protection measures",
+        ],
+      },
+      {
+        image: mobile2,
+        title:"iOS app Development",
+        descriptionLines: [
+          "Specialized in creating apps for",
+          "Seamless integration with existing systems and workflows",
+          "Built with scalability and flexibility for future growth",
+          "Ensures high security and data protection measures",
+        ],
+      },
+      {
+        image: mobile3,
+        title: "ANDriod app Development",
+        descriptionLines: [
+          "Tailored app solutions designed to meet unique business objectives",
+          "Seamless integration with existing systems and workflows",
+          "Built with scalability and flexibility for future growth",
+          "Ensures high security and data protection measures",
+        ],
+      },
+      {
+        image: mobile4,
+        title: "Cross-platform app Development",
+        descriptionLines: [
+          "Tailored app solutions designed to meet unique business objectives",
+          "Seamless integration with existing systems and workflows",
+          "Built with scalability and flexibility for future growth",
+          "Ensures high security and data protection measures",
+        ],
+      },
+      {
+        image: mobile5,
+        title: "enterprise mobility solutions",
+        descriptionLines: [
+          "Tailored app solutions designed to meet unique business objectives",
+          "Seamless integration with existing systems and workflows",
+          "Built with scalability and flexibility for future growth",
+          "Ensures high security and data protection measures",
+        ],
+      },
+    ],
+  },
+//   {
+//     image: web,
+//     title: "Web App Development",
+//     description:
+//       "Tailored web solutions for e-commerce, CMS, and single-page applications.",
+//     link: "/service/IT/web-app-development",
+//   },
+//   {
+//     image: backend,
+//     title: "Backend Development",
+//     description:
+//       "Robust backend solutions for APIs, database management, and cloud infrastructure.",
+//     link: "/service/IT/backend-development",
+//   },
+//   {
+//     image: maintainance,
+//     title: "Maintenance and Support",
+//     description:
+//       "Ensure optimal app performance with regular updates and troubleshooting.",
+//     link: "/service/IT/maintenance-and-support",
+//   },
+//   {
+//     image: consulting,
+//     title: "Consulting and Strategy",
+//     description:
+//       "Expert guidance for app strategy, technology choices, and requirement gathering.",
+//     link: "/service/IT/consulting-and-strategy",
+//   },
+//   {
+//     image: emerging,
+//     title: "Emerging Technologies",
+//     description:
+//       "Innovative apps with AR/VR, IoT, AI, and blockchain integration.",
+//     link: "/service/IT/emerging-technologies",
+//   },
+//   {
+//     image: testing,
+//     title: "Testing and Quality Assurance",
+//     description:
+//       "Comprehensive testing services to ensure high-performance, user-friendly apps.",
+//     link: "/service/IT/testing-and-quality-assurance",
+//   },
+];
