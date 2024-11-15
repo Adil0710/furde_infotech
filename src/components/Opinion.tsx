72c374ab90b208aee1da7b063f56b5f71b8916d8
Adil0710 <padil2246@gmail.com>
2024-11-15 12:16:59 +0530

    done

--- src/components/Opinion.tsx
+++ src/components/Opinion.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Button } from "./ui/button";
 import axios from "axios";
 import toast from "react-hot-toast";
